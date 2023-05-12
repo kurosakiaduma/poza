0a3915cccffeae5a15fa935b400f9331b55e2ab7
Tevin Aduma <tevin74@live.com>
2023-05-12 19:52:36 +0300

    include models with various validators for Doctors, Patients and Appointments

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import *
+from chat.models import Chat
+
 admin.site.register(Appointment)
-admin.site.admin_view(Appointment)
+admin.site.register(Doctor)
+admin.site.register(Chat)
--- booking/models.py
+++ booking/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 from datetime import datetime
-from django.contrib.auth.models import User
+from django.core.validators import *
+from django.contrib.admin import ModelAdmin
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
+from random import randint
+import PIL.Image, imageio
 
 SERVICE_CHOICES = (
-    ("Adult Cardiology", "Adult Cardiology"),
-    ("Adult Neurology", "Adult Neurology"),
-    ("Anaesthesia", "Anaesthesia"),
-    ("Anaesthesia and Critical Care Medicine", "Anaesthesia and Critical Care Medicine"),
-    ("Dermatology", "Dermatology"),
-    ("Ear, Nose and Throat (ENT)", "Ear, Nose and Throat (ENT)"),
-    ("General Surgery", "General Surgery"),
-    ("Gynaecology / Laparoscopic / Obsterics", "Gynaecology / Laparoscopic / Obsterics")
-    ("Nephrology", "Nephrology"),
-    ("Ophthalmologist", "Ophthalmologist"),
-    ("Paediatrics and Child Health", "Paediatrics and Child Health"),
-    ("Pain Management", "Pain Management"),
-    ("Physician /Internal Medicine", "Physician /Internal Medicine"),
-    ("Radiology", "Radiology"),
+    ("1", "Adult Cardiology"),
+    ("2", "Adult Neurology"),
+    ("3", "Anaesthesia"),
+    ("4", "Anaesthesia and Critical Care Medicine"),
+    ("5", "Dermatology"),
+    ("6, Nose and Throat (ENT)", "Ear, Nose and Throat (ENT)"),
+    ("7", "General Surgery"),
+    ("8", "Gynaecology / Laparoscopic / Obsterics"),
+    ("9", "Nephrology"),
+    ("10", "Ophthalmologist"),
+    ("11", "Paediatrics and Child Health"),
+    ("12", "Pain Management"),
+    ("13", "Physician /Internal Medicine"),
+    ("14", "Radiology"),
     )
 TIME_CHOICES = (
     ("3 PM", "3 PM"),
@@ -33,27 +37,63 @@ TIME_CHOICES = (
     ("7:30 PM", "7:30 PM"),
 )
 
+GENDER_CHOICES = (
+    ("Male","Male"),
+    ("Female", "Female"),
+    ("Non-Binary", "Non-Binary"),
+    ("Prefer Not To Say","Prefer Not To Say")
+)
+    
+class Patient(models.Model):
+    uuid = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    name = models.TextField(max_length=50, null=False)
+    birth_date = models.DateField(null=False, blank=True)
+    email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator(message="Please enter a valid email address in the format")], default="name@name.name")
+    gender = models.CharField(choices=GENDER_CHOICES, null=False, max_length=20)
+    phone_no = models.IntegerField(null=False)
+    account_type = models.CharField(max_length=10, default='PATIENT', editable=False)
+    kin_name = models.TextField(max_length=50, null=True)
+    kin_contact = models.IntegerField(null=True)
+    
+
+@receiver(post_save, sender=Patient)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Patient.objects.create(user=instance)
+
+@receiver(post_save, sender=Patient)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 class Appointment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    uuid = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
     service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Physician / Internal Medicine")
     day = models.DateField(default=datetime.now)
     time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
     time_ordered = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return f"{self.user.username} | day: {self.day} | time: {self.time}"
-    
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField()
-    birth_date = models.DateField(null=True, blank=True)
-    
-    
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+class Doctor(models.Model):
+    uuid = models.CharField(max_length=2, unique=True, blank=True, null=True)
+    password = models.CharField(max_length=30, validators=[MinLengthValidator(limit_value=8, message="Please ensure the password is at least 8 characters"), RegexValidator(regex='/password')], default="password")
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator(message="Please enter a valid email address in the format")], default="name@name.name")
+    phone_no = models.CharField(max_length=10, unique=True, validators=[RegexValidator(regex='^\d{10}$', message="Please enter a 10-digit number")])
+    account_type = models.CharField(max_length=10, default='DOCTOR', editable=False)
+    role = models.CharField(max_length=50, choices=SERVICE_CHOICES, null=False)
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        while True:
+            self.uuid = str(randint(1, 20))
+            if Doctor.objects.filter(uuid = self.uuid).count() < 1:
+                break
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('doctor-detail', kwargs={'pk': self.pk})
