752aee509826d12b57002ae583f5d094ec65bfe2
Tevin Aduma <tevin74@live.com>
2023-05-13 13:14:32 +0300

    resolved access to the booking page ||urls config fix

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('booking', views.booking, name='booking'),
+    path('bookings', views.booking, name='booking'),
     path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
     path('user-panel', views.userPanel, name='userPanel'),
     path('user-update/<int:id>', views.userUpdate, name='userUpdate'),
