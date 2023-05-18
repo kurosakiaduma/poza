d7c8e56e6729c5d31de35916a056302e1099b673
Tevin Aduma <tevin74@live.com>
2023-05-18 03:18:03 +0300

    fix uuid issue with doctor registration form

--- members/forms.py
+++ members/forms.py
@@ -1,13 +1,13 @@
 from tkinter import Widget
 from django.contrib.auth.forms import UserCreationForm
-from booking.models import Persona
+from booking.models import Persona, Doctor, SERVICE_CHOICES, GENDER_CHOICES, ACCOUNT_CHOICES
 from django import forms
 from django.db import models
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control form-control-lg', 'placeholder': 'example@gmail.com'}))
     name = forms.CharField(label='Name', max_length=50, widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
-    birth_date = forms.DateField(required=True, help_text="Enter using the YYYY-MM-DD format. Include hyphens.")
+    birth_date = forms.DateField(required=True, help_text="*Enter using the YYYY-MM-DD format. Include hyphens.")
     password1 = forms.CharField(label='password', widget=forms.PasswordInput)  
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
     
@@ -20,4 +20,24 @@ class RegisterUserForm(UserCreationForm):
             self.fields['name'].widget.attrs['class'] = 'form-control form-control-lg'
             self.fields['password1'].widget.attrs['class'] = 'form-control form-control-lg'
             self.fields['password2'].widget.attrs['class'] = 'form-control form-control-lg'
-        
+
+class RegisterDoctorForm(UserCreationForm):
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control form-control-lg', 'placeholder': 'example@gmail.com'}))
+    name = forms.CharField(label='Name', max_length=50, widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
+    birth_date = forms.DateField(required=True, help_text="*Enter using the YYYY-MM-DD format. Include hyphens.")
+    role = forms.ChoiceField(choices=SERVICE_CHOICES)
+    image = forms.FileField(widget=forms.FileInput({'class':'form-control form-control-lg'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)  
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = Doctor
+        fields = ('email', 'name', 'birth_date', 'account_type', 'gender', 'phone_no', 'role', 'image')
+
+    def __init__(self, *args, **kwargs):
+            super(RegisterDoctorForm, self).__init__(*args, **kwargs)
+            self.fields['name'].widget.attrs['class'] = 'form-control form-control-lg'
+            self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+            self.fields['image'].required = False
+            self.fields['password1'].widget.attrs['class'] = 'form-control form-control-lg'
+            self.fields['password2'].widget.attrs['class'] = 'form-control form-control-lg'
--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegisterUserForm
+from .forms import RegisterUserForm, RegisterDoctorForm
 from django.core.mail import send_mail
 
 def login_user(request):
@@ -11,14 +11,15 @@ def login_user(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user:
-            print(f"{username} {password} is being logged in as\n {user} {request.POST}")
+            print(f"{username} {password} has been authenticated as {user}.\nREQUEST.POST details --> {request.POST}")
             login(request, user, backend="members.customauthbackend.EmailAuthBackend")
-            print(f"Has been authenticated {user}")
+            print(f"Has been logged in {user}")
             # Redirect to a success page.
             return redirect('index')
         else:
             # Return an 'invalid login' error message.
-            messages.error(request, "There Was An Error!")
+            print(f"{user} Never authenticated")
+            messages.error(request, "Invalid email or password! Please try again.")
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
@@ -27,22 +28,39 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Sign Out Successful")
     return redirect('index')
-
+    
 def register_user(request):
     if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['email']
-            password = form.cleaned_data['password1']
-            print(f"{username} {password}")
-            user = authenticate(username=username, password = password)
-            print(f"{user}")
-            login(request, user)
-            messages.success(request, "Sign Up Completed!")
-            return redirect('index')
+        if request.user.is_superuser:
+            form =  RegisterDoctorForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['email']
+                password = form.cleaned_data['password1']
+                print(f"{username} {password}")
+                user = authenticate(username=username, password = password)
+                print(f"{user}")
+                login(request, user)
+                messages.success(request, "Sign Up Completed!")
+                return redirect('index')    
+        else:    
+            form = RegisterUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['email']
+                password = form.cleaned_data['password1']
+                print(f"{username} {password}")
+                user = authenticate(username=username, password = password)
+                print(f"{user}")
+                login(request, user)
+                messages.success(request, "Sign Up Completed!")
+                return redirect('index')
     else:
-        form = RegisterUserForm()
+        if request.user.is_superuser:
+            print(f"Form not valid")
+            form = RegisterDoctorForm()
+        else:
+            form = RegisterUserForm()
     return render(request, 'authenticate/register_user.html', {
         'form':form,
     })
