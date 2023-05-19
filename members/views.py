1fd75ba12a0e7529b72b997cd9929955a9bf7501
Tevin Aduma <tevin74@live.com>
2023-05-19 18:34:59 +0300

    Persona log times
    Included logic to capture user logout times to aid notifs logic for new
    notification display. Timezone restricted to Nairobi

--- members/views.py
+++ members/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.sessions.models import Session
+from django.shortcuts import render,redirect
+from datetime import datetime
 from .forms import RegisterUserForm, RegisterDoctorForm
-from django.core.mail import send_mail
+from booking.models import Persona
 
 def login_user(request):
     if request.method == "POST":
@@ -25,6 +27,36 @@ def login_user(request):
         return render(request, 'authenticate/login.html', {})
 
 def logout_user(request):
+    user = request.user
+    persona = Persona.objects.get(uuid=user.uuid)
+    """
+    Update the last activity time for the current user.
+
+    Args:
+        request (HttpRequest): The current HTTP request.
+    """
+    if request.user.is_authenticated:
+        # Get the session key for the current user
+        session_key = request.session.session_key
+
+        # Get the session object for the current user
+        session = Session.objects.get(session_key=session_key)
+
+        # Update the last activity time for the session
+         # Activate the GMT +3 timezone
+         # Get the Nairobi timezone object
+        import pytz
+        nairobi_tz = pytz.timezone('Africa/Nairobi')
+
+        session.last_activity = datetime.now(nairobi_tz)
+        session.save()
+        print(persona, session.last_activity)
+    
+    persona.last_logged = session.last_activity
+    persona.save()
+    
+    
+    
     logout(request)
     messages.success(request, "Sign Out Successful")
     return redirect('index')
