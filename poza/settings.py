e8c180679ce2b90b5fb1bf978eef1c01cf3bd73b
Tevin Aduma <tevin74@live.com>
2023-05-27 20:38:52 +0300

    staric files url fix

--- poza/settings.py
+++ poza/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = "/media/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'booking' / 'templates' / 'css',
     BASE_DIR / 'chat' / 'static',
